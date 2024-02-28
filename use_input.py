741dc64c67b1a705726a7d815f67923ab3426f01
Peter Kigia <peterkigia303@gmail.com>
2024-02-28 22:00:09 +0300

    first commit

---
+++ use_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("What is your location? ")
+print(f"Hello {name}, you are {age} years old and live in {location}.")
